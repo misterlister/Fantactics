768e6541d4f385c84abac4f1672e7c19c9bc1029
Hayden Lister <haydenglister@gmail.com>
2024-04-11 15:32:50 -0700

    Restructured startup logic

--- main.py
+++ main.py
@@ -9,34 +9,39 @@ from clientSender import Sender
 import socket
 
 doMainMenu = True
-online = False
+online = True
+sender = None
+conn = None
 this_file = "main.py"
 
 if __name__ == "__main__":
-
-    hostname = "Glen-Desktop"
-    ip_address = socket.gethostbyname(hostname)
-    port = 5000
-    onlineAvailable = True
-    connResult, conn = establishConn(ip_address, port)
-    if not connResult:
-        onlineAvailable = False
-
+    
     map = None
     for arg in argv:
         if arg == '-g':
             doMainMenu = False
+        if arg == '-o':
+            online = False
         if arg.startswith("-m:"):
             map = arg[3:]
+            
+    if online:
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        port = 5000
+        online = True
+        connResult, conn = establishConn(ip_address, port)
+        if not connResult:
+            online = False
+        else:
+            sender = Sender(conn)
 
     root = Tk()
     window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, root)
-    sender = Sender(conn)
-    game = Game(root,window,sender)
-    mainMenu = StartMenu(root, window, game, sender, onlineAvailable)
-    receiver = Receiver(conn,mainMenu)
+    game = Game(root, window, sender)
+    mainMenu = StartMenu(root, window, game, sender, online)
+    if online:
+        receiver = Receiver(conn, mainMenu)
 
-    #if doMainMenu: mainMenu = StartMenu(root, window, game, sender, online=online, map=map)
-    #else: game = Game(root, window, map=map)
     root.mainloop()
 
